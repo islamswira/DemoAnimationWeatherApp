d1b146e7df137ae683ffcce425db9e66c9c9605c
IslamSWIRA <islam.swira@flexxpay.com>
2022-11-10 19:29:49 +0200

    Initial Commit

---
+++ DemoAnimationWeatherApp/DemoAnimationWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAnimationWeatherAppApp.swift
+//  DemoAnimationWeatherApp
+//
+//  Created by IslamSwira on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoAnimationWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
